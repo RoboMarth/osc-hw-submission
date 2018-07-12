95d5e12e368898672a06a9e1363d1266fa655ecf
Michael Zhan <22856107+RoboMarth@users.noreply.github.com>
2018-07-12 11:19:21 -0400

    refactored to use struts and constants and other good things

--- app.rb
+++ app.rb
@@ -1,9 +1,103 @@
 require 'sinatra'
 require 'open3'
 require 'pathname'
+require 'etc'
+
+enable :sessions
+
+IDENTIFICATION_FILE = "this_is_a_homework_directory" # file found in HW directories to identify them as such
+PROJECTS_DIR = "/fs/project/" # directory contataining all project folders
+
+ClassInfo = Struct.new(:name, :project, :instructor, :assignments, :submissions, :date_created) do
+	def initialize (hw_dir_path)
+		self[:name] = hw_dir_path.basename.to_s
+		self[:project] = hw_dir_path.dirname.basename.to_s
+		self[:instructor] = Etc.getpwuid(hw_dir_path.stat.uid).name
+		self[:assignments] = hw_dir_path.children.count - 2 # minus 2 because of scripts directory and homework dir file 
+		self[:submissions] = hw_dir_path.children.flat_map {|pc| pc.children if pc.directory?}.compact.count{|x| x.directory?} 
+		self[:date_created] = (hw_dir_path + IDENTIFICATION_FILE).mtime
+	end
+end
+
+AssignmentInfo = Struct.new(:name, :submissions, :date_due)
+
+Message = Struct.new(:type, :text) # type corresponds to bootstrap theme colors: success, danger, warning, info
+
+helpers do
+	def redirect_back_with_msgs(messages)
+		session[:msgs] = messages
+		redirect back	
+	end
+end
+
+get '/' do
+	redirect to '/all'
+end
+
+get '/all' do
+	@table_rows = Array.new # store ClassInfo objects
+	@msgs = session[:msgs] # store any Messages that need to be displayed (i.e errors)
+	@project_paths = Array.new
+
+	# get groups
+	stdout_str, stderr_str, status = Open3.capture3("groups")
+	@msg.push Message.new("danger", "could not identify user's groups (groups)") unless status.success?
+	groups = stdout_str.chomp.split
+	
+	# look for project directories under /fs/project/
+	groups.each do | g |
+		path = Pathname.new(PROJECTS_DIR).join(g)
+		@project_paths.push path if path.directory?
+	end
+		
+	@msgs.push Message.new("danger", "user does have access to any project directories") if @project_paths.empty?
+
+	# look for hw directories in the project directories
+	@project_paths.each do | pp |
+		Pathname.glob(pp + "*" + IDENTIFICATION_FILE) do | p |
+			hw_dir_path = p.dirname	
+			@table_rows.push ClassInfo.new(hw_dir_path)
+		end
+	end
+
+	@table_rows = @table_rows.sort_by{|c| c.date_created}.reverse!
+	
+	erb :index
+end
+
+get '/all/:project' do
+	@table_rows = Array.new
+	@project = params[:project]
+
+	pn = Pathname.new(PROJECTS_DIR).join(@project)
+	halt 404, "File or directory not found" unless pn.exist?
+	halt 403, "Permission denied" unless pn.readable?
+
+	Pathname.glob(pn + "*" + IDENTIFICATION_FILE) do | p |
+		hw_dir_path = p.dirname
+		@table_rows.push ClassInfo.new(hw_dir_path)
+	end
+
+	@table_rows = @table_rows.sort_by{|c| c.date_created}.reverse!
+
+	erb :project
+end
+
+get '/all/:project/:class' do
+	@dir_paths = Array.new # paths to hw submissions (e.g. /fs/project/PZS0530/some_class/osc0001)
+	@project = params[:project]
+	@class = params[:class]
+
+	pn = Pathname.new(PROJECTS_DIR).join(@project).join(@class)
+	halt 404, "File or directory not found" unless pn.exist?
+	halt 403, "Permission denied" unless pn.readable?
+	halt 401, "Not a homework directory" unless (pn + IDENTIFICATION_FILE).exist?
+
+	erb :class	
+end
 
 get '/download/*' do | glob |
-	pn = Pathname.new("/fs/project/#{glob}")
+	pn = Pathname.new(PROJECTS_DIR).join(glob)
 	halt 404, "File or directory not found" unless pn.exist?
 	halt 403, "Permission denied" unless pn.readable?
 
@@ -32,12 +126,11 @@ get '/download/*' do | glob |
 		send_file tmp_filepath.to_s, :filename => tmp_filepath.basename.to_s
 
 	else
-		"Not a file or directory"
+		halt 400, "Not a file or directory"
 	end
 end	
 
-# TODO: use better route name and redirect back with session containing errors
-post '/' do
+post '/add/class' do
 	pn = Pathname.new(params[:parent_dir])
 	
 	halt 404, "Parent directory not found" unless pn.exist?
@@ -60,74 +153,9 @@ post '/' do
 		halt 500, "could not create hw directory: #{stdout}" unless status.success?		
 	end
 	
-	redirect to '/'
+	redirect back
 end
 
-get '/' do
-	@errors = Array.new # store any errors that occur
-	@dir_paths = Array.new # store paths to found homework directories
-
-	# get user
-	stdout_str, stderr_str, status = Open3.capture3("whoami")
-	@errors.push "could not establish identity (whoami)" unless status.success?
-	@user = stdout_str.chomp
-
-	# get groups
-	stdout_str, stderr_str, status = Open3.capture3("groups")
-	@errors.push "could not identify user's groups (groups)" unless status.success?
-	groups = stdout_str.chomp.split
-	
-	# look for project directories under /fs/project/
-	@project_paths = Array.new
-	groups.each do | g |
-		path = Pathname.new("/fs/project").join(g)
-		@project_paths.push path if path.directory?
-	end
-		
-	@errors.push "user does have access to any project directories" if @project_paths.empty?
-
-	# look for hw directories in the project directories
-	@project_paths.each do | p |
-		Pathname.glob(p + "*" + "this_is_a_homework_directory") do | p2 |
-			@dir_paths.push p2.dirname if p2.file?
-		end
-	end
-
-	@dir_paths = @dir_paths.sort_by{|p| (p + "this_is_a_homework_directory").mtime}.reverse!
-	
-	if @errors.empty?
-		erb :index
-	else
-		@errors
-	end
-end
-
-get '/:project' do
-	@dir_paths = Array.new
-	@project = params[:project]
-
-	pn = Pathname.new("/fs/project/#{@project}")
-	halt 404, "File or directory not found" unless pn.exist?
-	halt 403, "Permission denied" unless pn.readable?
-
-	Pathname.glob(pn + "*" + "this_is_a_homework_directory") do | p |
-		@dir_paths.push p.dirname
-	end
-
-	@dir_paths = @dir_paths.sort_by{|p| (p + "this_is_a_homework_directory").mtime}.reverse!
-
-	erb :project
-end
-
-get '/:project/:class' do
-	@dir_paths = Array.new # paths to hw submissions (e.g. /fs/project/PZS0530/some_class/osc0001)
-	@project = params[:project]
-	@class = params[:class]
-
-	pn = Pathname.new("/fs/project/#{@project}/#{@class}")
-	halt 404, "File or directory not found" unless pn.exist?
-	halt 403, "Permission denied" unless pn.readable?
-	halt 401, "Not a homework directory" unless (pn + "this_is_a_homework_directory").exist?
-
-	erb :class	
+post '/add/assignment' do
+	redirect back
 end
